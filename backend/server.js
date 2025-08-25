64d41a3451f3b69788c6415d716aa40263d0f3a5
Marko <nikolicmarko035@gmail.com>
2025-08-25 02:06:18 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
+
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Catch-all: return React's index.html for any unknown paths
+app.get("/*splat", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
